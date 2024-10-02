3b0f3858ecbdeead7bb8a607ddba6d3b46ea43e6
OgurecA <acpwork044@gmail.com>
2024-10-03 01:43:45 +0400

    k

--- Natravlivatel3000.js
+++ Natravlivatel3000.js
@@ -1,4 +1,4 @@
-const db = require('./Database.js'); // Подключение к базе данных
+const db = require('./Database.js');
 const schedule = require('node-schedule');
 
 // Функция для удаления устаревших заявок
@@ -10,17 +10,19 @@ function removeExpiredRequests() {
 
   // Удаление устаревших записей из таблицы `search`
   const deleteSearch = db.prepare(`DELETE FROM search WHERE deletion <= ?`);
-  deleteSearch.run(formattedCurrentDate);
+  const changesSearch = deleteSearch.run(formattedCurrentDate);
+  console.log(`Удалено ${changesSearch.changes} устаревших записей из таблицы search.`);
 
   // Удаление устаревших записей из таблицы `offer`
   const deleteOffer = db.prepare(`DELETE FROM offer WHERE deletion <= ?`);
-  deleteOffer.run(formattedCurrentDate);
-
-  console.log('Удаление устаревших заявок завершено.');
+  const changesOffer = deleteOffer.run(formattedCurrentDate);
+  console.log(`Удалено ${changesOffer.changes} устаревших записей из таблицы offer.`);
 }
 
-// Планируем выполнение функции каждые 15 минут
-schedule.scheduleJob('*/15 * * * *', removeExpiredRequests);
+// Запускаем функцию проверки каждые 15 минут
+schedule.scheduleJob('*/15 * * * *', () => {
+  console.log('Запуск проверки устаревших заявок...');
+  removeExpiredRequests();
+});
 
-// Запуск функции при старте
-removeExpiredRequests();
+console.log('Планировщик запущен и готов к работе.');
